5b4caaefd0c903df7f859aaed4338f82d63f0597
Gaston Heaps <gaston.heaps@gmail.com>
2022-09-11 10:18:41 -0700

    results query

--- app/controllers/results.controller.js
+++ app/controllers/results.controller.js
@@ -6,10 +6,10 @@ exports.getResults = (req, res) => {
   const id = req.query["postId"];
 
   Result.findOne({postId: id}, 'data')
-    .then(results => {
-      if (!results)
+    .then(data => {
+      if (!data)
         res.status(404).send({ message: "Not found Results with id " + id });
-      else res.send(results);
+      else res.send(data);
     })
     .catch(err => {
       res
@@ -26,22 +26,21 @@ exports.postResults = (req, res) => {
     });
   }
 
-
-    const result = new Result({
-      postId: req.body.postId,
-      surveyResult: req.body.surveyResult
-    });
-  
     // Save survey in the database
-    result
-      .save(result)
+    Result.findOneAndUpdate(
+      {postId: req.body.postId}, 
+      {$push: {"data": req.body.surveyResult}},
+      {safe: true, upsert: true, new : true})
       .then(data => {
-        res.send(data);
+        if (!data) {
+          res.status(404).send({
+            message: `Cannot update Survey with id=${id}. Maybe Survey was not found!`
+          });
+        } else res.send({ message: "Survey was updated successfully." });
       })
       .catch(err => {
         res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the survey."
+          message: "Error updating Survey with id=" + id
         });
       });
 };
